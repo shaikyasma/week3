1b75e65b99fa1e918bc2dc7f3b26eba67de958c0
shaikyasma <23c11a05h4@anurag.ac.in>
2025-07-29 11:20:50 +0530

    Add files via upload

---
+++ registration form.c
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>welcome to bookstagram website</title>
+<scripttype="text/java script"></script>
+</head>
+<body>
+<center>
+<h1>regisration form</h1>
+</br>
+<table cellpadding="1"align='center'>
+<tr><td><b>user name:*</b></td>
+<td><inputtype="password"name="password"></td></tr>
+<tr><td><b><email id:*</b></td>
+<td><input type="text"name"email"></td></tr>
+<tr><td><b>phoneno:*</b></td>
+<td><input type="text"name="phoneno"></td></tr>
+<tr><td ralign="top"><b>gender:*</b></td>
+<td><input type="radio"name="gen"value="female"><b>female</b></td></tr>
+<tr><td valign="top"><b>language known:*</b></td>
+<td><input type="check box"name="lang"value="english">english<br>
+<td><input type="check box"name="lang"value="telugu">telugu<br>
+<td><input type="check box"name="lang"value="hindi<br>
+</td></tr>
+<tr><td valign+"top"><b>address:*</b></td>
+<td><text area name="address"></text area></td></tr>
+<tr><td><input types="button"value="submit"hspace="10"onclick="validate()">
+<input type="reset"value="reset"></td></tr>
+<tr><td colspon=2>*<font color="red">fields are mandatory</font>
+</td>
+</tr>
+</table>
+</from>
+</center>
+</body>
+</html>
